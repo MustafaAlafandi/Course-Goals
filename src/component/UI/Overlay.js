dab5d94aa8179ace0f025a6278ef6916dc5ec2a3
mustafa alafandi <mustafaalafandi1194@gmail.com>
2025-04-09 21:45:06 +0300

    delete some files

--- src/component/UI/Overlay.js
+++
@@ -1,9 +0,0 @@
-import "./Overlay.css";
-import ReactDOM from 'react-dom';
-const OL = ()=>{
-    return <div className="overlay"></div>;
-}
-const Overlay = ()=>{
-    return ReactDOM.createPortal(<OL/>,document.getElementById("overlay-root"));
-}
-export default Overlay;
--- src/component/Users/Users.js
+++
@@ -1,9 +0,0 @@
-import './Users.css';
-const Users = (props)=>{
-    return(
-        <div className="users">
-        {props.users.map((user,i)=><p key={i} className="users__user">{`${user.name}(${user.age} years old)`}</p>)}
-        </div>
-    )
-}
-export default Users;
